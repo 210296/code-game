298d95ff6a6ae37c8a9edc8eac46ce8d2c87a752
210296 <210296@hkis.edu.hk>
2016-05-19 09:10:32 +0800

    Finished CSS and started jQuery

--- gameTemplate1/gamedev.js
+++ gameTemplate1/gamedev.js
@@ -1,5 +1,22 @@
 $( document ).ready(function() {
 
 //Your jquery goes here
+$(document).ready(function(){
+
+$("#two").hide();
+$("#three").hide();
+$("#four").hide();
+$("#five").hide();
+$("#six").hide();
+$("#seven").hide();
+$("#eight").hide();
+$("#nine").hide();
+$("#ten").hide();
+$("#eleven").hide();
+$("#twelve").hide();
+$("#thirteen").hide();
+$("#fourteen").hide();
+$("#fifteen").hide();
+});
 
 });
